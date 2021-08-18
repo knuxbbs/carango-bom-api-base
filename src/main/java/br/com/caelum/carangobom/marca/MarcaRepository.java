d2b18a048fa165d809e1c98fe2c0d651e0059967
graysoncunha <grayson.silva@hotmail.com>
2021-08-18 12:16:21 -0300

    fix(marca): altera repositório de marca para usar o spring data (#1)

--- src/main/java/br/com/caelum/carangobom/marca/MarcaRepository.java
+++ src/main/java/br/com/caelum/carangobom/marca/MarcaRepository.java
@@ -1,38 +1,14 @@
 package br.com.caelum.carangobom.marca;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import javax.persistence.EntityManager;
 import java.util.List;
 import java.util.Optional;
 
-@Repository
-public class MarcaRepository {
+public interface MarcaRepository extends JpaRepository<Marca, Long> {
 
-    private EntityManager em;
+    public Optional<Marca> findById(Long id);
 
-    @Autowired
-    public MarcaRepository(EntityManager em) {
-        this.em = em;
-    }
-
-    public void delete(Marca marca) {
-        em.remove(marca);
-    }
-
-    public Marca save(Marca marca) {
-        em.persist(marca);
-        return marca;
-    }
-
-    public Optional<Marca> findById(Long id) {
-        return Optional.ofNullable(em.find(Marca.class, id));
-    }
-
-    public List<Marca> findAllByOrderByNome() {
-        return em.createQuery("select m from Marca m order by m.nome", Marca.class)
-                .getResultList();
-    }
+    public List<Marca> findAllByOrderByNome();
 
 }
