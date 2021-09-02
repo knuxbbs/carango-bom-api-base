5b2bbb4536c49f51e7ee73d9b6ca9f690b5f0238
Bruno Bacelar <knuxbbs@gmail.com>
2021-09-02 16:38:48 -0300

    Remove referencia ao lombok

--- src/main/java/br/com/caelum/carangobom/erro/Erro.java
+++ src/main/java/br/com/caelum/carangobom/erro/Erro.java
@@ -1,12 +1,31 @@
 package br.com.caelum.carangobom.erro;
 
 import org.springframework.http.HttpStatus;
-import lombok.Data;
-import lombok.RequiredArgsConstructor;
 
-@Data
-@RequiredArgsConstructor
 public class Erro {
-    private final HttpStatus httpStatus;
-    private final String message;
+
+  private HttpStatus httpStatus;
+  private String message;
+
+  public Erro(HttpStatus httpStatus, String message) {
+    this.httpStatus = httpStatus;
+    this.message = message;
+  }
+
+  public HttpStatus getHttpStatus() {
+    return httpStatus;
+  }
+
+  public void setHttpStatus(HttpStatus httpStatus) {
+    this.httpStatus = httpStatus;
+  }
+
+  public String getMessage() {
+    return message;
+  }
+
+  public void setMessage(String message) {
+    this.message = message;
+  }
+
 }
