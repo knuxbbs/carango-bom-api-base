5c8b19262c152a32a1f99086447f7b7722b7ce62
Bruno Bacelar <knuxbbs@gmail.com>
2021-09-15 16:07:03 -0300

    chore: pequenos ajustes

--- src/main/java/br/com/caelum/carangobom/config/CorsConfiguration.java
+++ src/main/java/br/com/caelum/carangobom/config/CorsConfiguration.java
@@ -1,20 +1,15 @@
 package br.com.caelum.carangobom.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfiguration {
+public class CorsConfiguration implements WebMvcConfigurer {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedMethods("*");
-            }
-        };
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("*");
     }
+
 }
